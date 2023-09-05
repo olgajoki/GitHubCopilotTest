8297977b17eed8e93dad72be63a702ee86485d92
olgajokinen <olga.jokinen@ramirent.fi>
2023-09-05 16:49:52 +0300

    initiating copilot test app

---
+++ pages/api/server.js
@@ -0,0 +1,9 @@
+/*Create a server with the following specifications:
+
+1. import express and dotenv node modules
+3. create the server with express and name it app
+4. use port 8080 as default port
+5. enable body parser to accept json data
+6. state which port the server is listening to and log it to the console
+*/
+
