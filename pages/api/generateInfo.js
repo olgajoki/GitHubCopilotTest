5b653c5e49c47910c577b6d78ee850faa3656ab9
olgajokinen <olga.jokinen@ramirent.fi>
2023-09-06 10:19:22 +0300

    router added, constructor problem in generateInfo

--- pages/api/generateInfo.js
+++ pages/api/generateInfo.js
@@ -5,54 +5,51 @@ Create a controller with the following specifications:
 2. create a new configuration object that includes the api key and uses the Configuration class from the openai module
 3. create a new instance of the OpenAIApi class and pass in the configuration object
 4. create an async function called generateInfo that accepts a request and response object as parameters
-5. use try to make a request to the OpenAI completetion api and return the response
-6. use catch to catch any errors and return the error include a message to the user
+5. use try to make a request to the openai api and return the response
+6. use catch to catch any errors and return the error
 7. export the generateInfo function as a module
 */
 
+const { Configuration, OpenAIApi } = require('openai');
+// import json data from prompt.json file
+const { recipePrompt } = require("../../data/prompt.json");
 
-
-const { recipePrompt } = require("../../data/recipe.json");
-const { Configuration, OpenAIApi } = require("openai");
-
-const config = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
 });
-const openai = new OpenAIApi(config);
+
+const openai = new OpenAIApi(configuration);
+
 
 const generateInfo = async (req, res) => {
     const { recipe } = req.body;
-
-  try {
-    const completion = await openai.createChatCompletion({
+  
+    try {
+      const completion = await openai.createChatCompletion({
         model: "gpt-3.5-turbo",
         messages: [{ role: "user", content: `${recipePrompt}${recipe}` }],
         max_tokens: 200,
         temperature: 0,
         n: 1,
+      });
+      const response = completion.data.choices[0].message.content;
+  
+      return res.status(200).json({
+        success: true,
+        data: response,
+      });
+    } catch (error) {
+      console.log(error);
+      if (error.response && error.response.status === 401) {
+        return res.status(401).json({
+          error: "Please provide a valid API key.",
         });
-        const response = completion.data.choices[0].message.content;
-
-
-        return res.status(200).json({
-          success: true,
-          data: response,
-        });
-
-  } catch (error) {
-    console.log(error);
-    if (error.response.status === 401) {
-      return res.status(401).json({
-        error: "Please provide a valid API key.",
+      }
+      return res.status(500).json({
+        error:
+          "An error occurred while generating recipe information. Please try again later.",
       });
     }
-    return res.status(500).json({
-      error:
-        "An error occurred while generating recipe information. Please try again later.",
-    });
-  }
-
- 
-};
-
+  };
+  
 module.exports = { generateInfo };
--- pages/api/server.js
+++ pages/api/server.js
@@ -1,9 +1,27 @@
-/*Create a server with the following specifications:
+/* 
+Create a server with the following specifications:
 
-1. import express and dotenv node modules
-3. create the server with express and name it app
-4. use port 8080 as default port
-5. enable body parser to accept json data
-6. state which port the server is listening to and log it to the console
-*/
+1. Use express node module and import it
+2. Create a server with express and name it app
+3. Get the port from the environment variable PORT or use 8080
+4. enable body parser to accept json data
+5. state which port the server is listening to
+*/ 
 
+const express = require('express');
+const dotenv = require('dotenv').config();
+const PORT = 8080
+
+const app = express();
+
+// enable body parser to accept json data
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// add router to the server and name it openai
+app.use('/openai', require('./router'));
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+// export the express api
+module.exports = app;
